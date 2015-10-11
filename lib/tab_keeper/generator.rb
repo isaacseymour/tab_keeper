59ae3b2ca0c4a1d289bc6fcc0a8e43a0ea37d6b9
Isaac Seymour <i.seymour@oxon.org>
2015-10-11 14:04:12 +0100

    Pass timing into to pipeline components

--- lib/tab_keeper/generator.rb
+++ lib/tab_keeper/generator.rb
@@ -7,7 +7,7 @@ module TabKeeper
     def generate(job_list, **options)
       job_list.map do |job, timing|
         timing + " " + @pipeline.reduce(nil) do |previous, pipe|
-          pipe.new(previous, job: job, **options).to_s
+          pipe.new(previous, job: job, timing: timing, **options).to_s
         end
       end.join("\n\n") + "\n"
     end
